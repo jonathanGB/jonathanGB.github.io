098d0f84046bc96904dc0ade899284bc5ffd3901
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-09-27 17:27:10 -0400

    Content for education added - started foundationjs - ...
    reduced the foundation js file to the tooltip library - added
    modernizrjs - back to top of the page arrow added, which hides if close
    to the top of the page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(function() {
+	$(document).foundation({
+		tooltip: {
+			touch_close_text: ''
+		}
+	});
+
+	$('#arrowTop img').click(function(e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: 0
+		}, window.scrollY * 2 / 3);
+	});
+
 	$('a.smooth-scroll').click(function(e) {
 		e.preventDefault();
 		
@@ -7,6 +21,16 @@ $(function() {
 
 		$('html, body').animate({
 			scrollTop: $goToElement.offset().top - navHeight
-		}, Math.abs(window.scrollY - $goToElement.offset().top ));
+		}, Math.abs(window.scrollY - $goToElement.offset().top));
 	});
+
+	$(document).scroll(function(e) {
+		if (scrollY > 300)
+			$('#arrowTop').show();
+		else
+			$('#arrowTop').hide();
+	});
+
+	if (scrollY > 300)
+		$('#arrowTop').show();
 })
