5c0006fd4ca1e6ed48a4a0fd97fa04d1836590d5
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-09-27 00:22:07 -0400

    Enhance smoothScroll
    Better final positioning, constant speed of scroll

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ $(function() {
 		e.preventDefault();
 		
 		var $goToElement = $($(this).attr('href'));
+		var navHeight = $('nav').height() + parseInt($('nav').css('padding-bottom')) + parseInt($('nav').css('padding-top'));
 
 		$('html, body').animate({
-			scrollTop: $goToElement.offset().top
-		}, 1500);
+			scrollTop: $goToElement.offset().top - navHeight
+		}, Math.abs(window.scrollY - $goToElement.offset().top ) * 2);
 	});
 })
