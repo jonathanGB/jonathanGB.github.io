bb72bb74ed6c2f93a2c43cd13960063446643878
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-09-28 15:46:26 -0400

    Added lang attribute - dynamic "last update" info
    scrollReveal for skillsList

--- js/script.js
+++ js/script.js
@@ -33,6 +33,27 @@ $(function() {
 		$('a[data-dropdown]').removeAttr('data-options');
 	}
 
+	$.get("https://api.github.com/repos/jonathanGB/jonathanGB.github.io", function(data) {
+		var lastUpdate = new Date(data.pushed_at),
+		    lang = $('html').attr('lang'),
+		    days = [],
+		    months = [],
+		    lastUpdateString = '';
+		    
+		if (lang == "en") {
+			days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+			lastUpdateString = lastUpdateString.concat(days[lastUpdate.getDay()], ", ", months[lastUpdate.getMonth()], " ", lastUpdate.getDate(), " ", lastUpdate.getFullYear());
+		} else if (lang == "fr") {
+			days = [ "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+			months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+			lastUpdateString = lastUpdateString.concat(days[lastUpdate.getDay()], ", ", lastUpdate.getDate(), " ", months[lastUpdate.getMonth()], " ", lastUpdate.getFullYear());
+		}
+
+		$('footer #lastPush').html(lastUpdateString);
+		$('footer').show();
+	});
+
 	$(document).foundation({
 		tooltip: {
 			touch_close_text: '',
